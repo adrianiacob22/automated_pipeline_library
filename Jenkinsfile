a184257b4733a65d25a87aaf482fa3626f4bf0e9
Adrian Iacob <adrian@oc2627300782.ibm.com>
2020-04-20 20:36:54 +0300

    updated jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,10 +4,34 @@ pipeline {
        // Rather than inline YAML, in a multibranch Pipeline you could use: yamlFile 'jenkins-pod.yaml'
        // Or, to avoid YAML:
         containerTemplate {
-           name 'shell'
-            image 'ubuntu'
-            command 'sleep'
-            args '50'
+         metadata:
+           labels:
+             app: build
+          spec:
+           containers:
+           - name: jnlp
+             image: nexus.local.net:8123/jenkins-jnlp-slave:20200420
+             env:
+             - name: CONTAINER_ENV_VAR
+               value: jnlp
+           - name: maven
+             image: maven:3.3.9-jdk-8-alpine
+             command:
+             - cat
+             tty: true
+             env:
+             - name: CONTAINER_ENV_VAR
+               value: maven
+           - name: busybox
+             image: busybox
+             command:
+             - cat
+             tty: true
+             env:
+             - name: CONTAINER_ENV_VAR
+               value: busybox
+           imagePullSecrets:
+           - name: docker-repo
         }
    }
    }
--- scripts/scratch.sh
+++ scripts/scratch.sh
@@ -103,5 +103,114 @@ spec:
       type: "maven"
 EOF
 
+# cat <<EOF | kubectl apply -f -
+# kind: Service
+# apiVersion: v1
+# metadata:
+#  name: jenkins
+#  namespace: build
+# spec:
+#  type: ExternalName
+#  externalName: jenkins.local.net
+# EOF
+#
+# cat <<EOF | kubectl apply -f -
+# kind: Service
+# apiVersion: v1
+# metadata:
+#  name: jenkins
+#  namespace: build
+#  labels:
+#    ep: jenkins
+# spec:
+#  ports:
+#  - port: 27443
+#    targetPort: 443
+# ---
+# kind: Endpoints
+# apiVersion: v1
+# metadata:
+#  name: jenkins
+#  namespace: build
+#  labels:
+#    ep: jenkins
+# subsets:
+#  - addresses:
+#      - ip: 192.168.100.3
+#    ports:
+#      - port: 443
+# EOF
+
+
+
+
+
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: dnsutils
+spec:
+  containers:
+  - name: dnsutils
+    image: gcr.io/kubernetes-e2e-test-images/dnsutils:1.3
+    command:
+      - sleep
+      - "3600"
+    imagePullPolicy: IfNotPresent
+  restartPolicy: Always
+EOF
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: openssl
+spec:
+  containers:
+  - command:
+    - sleep
+    - "3600"
+    image: frapsoft/openssl
+    imagePullPolicy: IfNotPresent
+    name: openssl
+  restartPolicy: Always
+EOF
+
 ## when pvc is not binding to a released pv
 kubectl patch pv maven -p '{"spec":{"claimRef": null}}'
+
+##
+echo "-----BEGIN CERTIFICATE-----
+MIIDYjCCAkqgAwIBAgIIF+Dqjah0m08wDQYJKoZIhvcNAQELBQAwFTETMBEGA1UE
+AxMKa3ViZXJuZXRlczAeFw0yMDA0MTcwOTI4MDhaFw0yMTA0MTcwOTI4MDhaMBkx
+FzAVBgNVBAMTDmt1YmUtYXBpc2VydmVyMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8A
+MIIBCgKCAQEAyaUHbLqMTsq4Ijhf6LSyei68DTF9qyqERjy4S924SG79oD4ee9E3
+l3AwvaWJryv0ktieED3fvl5nRf5/C19/BQSaYPHHNmLCWBc8n1ZVI8VdvZBsvYHQ
+bBC2KZeuCnMwHKWHMOpZ9gohirVirJEeF/WCfqUi/R0X0KokkTRxr2heymaCT8tH
+TvPTqE/PUqvGfsXMBQoRhaARoDxJOVVf729nMHik1Drb8wt6BbWzMj45gRBvfyLx
+c+VZHuWwKP83tz2dByOeD1jWrD6nAl3EY8IRWwAu2lAvrGCumkCDSS8cEmgsPhwq
+Pqsq6jQWpMzrlc3Vzxm2kAukfLF/yK7L2wIDAQABo4GxMIGuMA4GA1UdDwEB/wQE
+AwIFoDATBgNVHSUEDDAKBggrBgEFBQcDATCBhgYDVR0RBH8wfYIRa21hc3Rlci5s
+b2NhbC5uZXSCCmt1YmVybmV0ZXOCEmt1YmVybmV0ZXMuZGVmYXVsdIIWa3ViZXJu
+ZXRlcy5kZWZhdWx0LnN2Y4Ika3ViZXJuZXRlcy5kZWZhdWx0LnN2Yy5jbHVzdGVy
+LmxvY2FshwQKYAABhwSsKipkMA0GCSqGSIb3DQEBCwUAA4IBAQCMDkq6nIqqK/Pr
+IEocup/veFMwT8ywmr9RlDOFbW1oNtdr31x8XuSkBNsG0nHk4QLSfXrVh2fhbKR9
+SMJPPjHfgrdJXeoTOG5AV74vuPwK0HMJHCvzUxjlem3ZRJa59NfsQdhZ8nVzohd5
+Luszi7SAn1TX5mDWYegluHeH28/4BkZPo4685w1/hdjWwRcgZ1YMCvmuVsqIKjeQ
+b505wDwmfE/f7QjS50Irgwbi4Y74Q41DNCShtRwRwLdvWM5hJdWEAqMxNLCUxXlo
+3faPXNMRtztAaW2U4raSUmozUoyr/36/+OFaxl89KGcJfxXo5pcqmGtzWdpoKZv1
+UFQL797q
+-----END CERTIFICATE-----" > /tmp/kube.der
+
+keytool -importcert -alias jenkins-CA -keystore /usr/local/openjdk-11/lib/security/cacerts -file /tmp/kube.der -storepass changeit -noprompt
+
+# tag and push docker images to nexus
+docker build -t jenkins-jnlp-slave .
+docker tag jenkins-jnlp-slave:latest nexus.local.net:8123/jenkins-jnlp-slave:20200420
+docker tag nexus.local.net:8123/jenkins-jnlp-slave:20200420 nexus.local.net:8123/jenkins-jnlp-slave:latest
+docker push nexus.local.net:8123/jenkins-jnlp-slave:20200420
+docker push nexus.local.net:8123/jenkins-jnlp-slave:latest
+
+# Create docker registry secret in Kubernetes
+ kubectl create secret generic docker-repo --from-file=.dockerconfigjson=/home/adrian/.docker/config.json --type=kubernetes.io/dockerconfigjson
